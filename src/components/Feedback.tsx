bb447d2d07d988137c148994526238334468678f
hemaalatha-nk <hemaalatha.nk@gmail.com>
2023-09-28 15:19:11 -0500

    Updated Innitial data

--- src/components/Feedback.tsx
+++ src/components/Feedback.tsx
@@ -4,31 +4,36 @@ import { AddRequestForm } from "./FeedbackRequest";
 import { RequestList } from "./FeedbackRequest";
 import { useState } from "react";
 import { FormData } from "../utils/types";
+import initialData from "../initial-data.json";
+// import {initial-data} from "../initial-data";
 
 function FeedbackScreen() {
     // There maybe a need of defining state and passing them as props to components for enabling state sharing across components.
     const [reqCompleted, setStatus] = useState<Array<number>>([]);
-    const [data, setData] = useState<Array<FormData>>([
-        {
-            name: "Crish",
-            emailId: "crish4@uic.edu",
-            ldescription: "Schema",
-            sdescription:
-                "The existing colo schema cam be made more attractive.",
-        },
-        {
-            name: "Hema",
-            emailId: "hning4@uic.edu",
-            ldescription: "UI",
-            sdescription: "Submit button is not rendering properly.",
-        },
-        {
-            name: "Jhon",
-            emailId: "jhon4@uic.edu",
-            ldescription: "Features Request",
-            sdescription: "Make it easy.",
-        },
-    ]);
+    const [data, setData] = useState<Array<FormData>>(
+        initialData,
+        //   [
+        //     {
+        //         name: "Crish",
+        //         emailId: "crish4@uic.edu",
+        //         ldescription: "Schema",
+        //         sdescription:
+        //             "The existing colo schema cam be made more attractive.",
+        //     },
+        //     {
+        //         name: "Hema",
+        //         emailId: "hning4@uic.edu",
+        //         ldescription: "UI",
+        //         sdescription: "Submit button is not rendering properly.",
+        //     },
+        //     {
+        //         name: "Jhon",
+        //         emailId: "jhon4@uic.edu",
+        //         ldescription: "Features Request",
+        //         sdescription: "Make it easy.",
+        //     },
+        // ]
+    );
 
     return (
         <>
